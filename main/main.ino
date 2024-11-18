1b431a782b91bd533457df0d11d12ca912066cc3
omarelfouly <omarelfouly@aucegypt.edu>
2024-11-18 16:19:35 +0200

    initial commit

---
+++ main/main.ino
@@ -0,0 +1,35 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "car";
+const char* password = "123456789";
+IPAddress local_ip(192,168,4,1);
+IPAddress gateway(192,168,4,1);
+IPAddress subnet(255,255,255,0);
+
+WiFiServer server(80);
+WiFiClient persistentClient;
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  WiFi.softAP(ssid, password);
+  
+  server.begin();
+}
+
+void loop() {
+  if (!persistentClient.connected()) {
+    persistentClient = server.available();
+  }
+  
+  if (persistentClient && persistentClient.available()) {
+    String data = persistentClient.readStringUntil('\n');
+    data.trim();
+    
+    if (data.length() > 0 && !data.startsWith("GET") && !data.startsWith("User")&& !data.startsWith("Host")&& !data.startsWith("Accept")&& !data.startsWith("Connection")) {
+      Serial.println(data);
+    }
+  }
+}
